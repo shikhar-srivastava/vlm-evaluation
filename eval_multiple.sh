00ca3b057fc2bd4fd3dfc83e1491c64870c5905e
shikhar-srivastava <shikharsrivastav@gmail.com>
2024-05-06 11:27:12 -0400

    update

--- eval_multiple.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# List of model names
-MODEL_NAMES=("model1" "model2" "model3")
-
-# Loop over each model name and call eval_cl.sh with it
-for MODEL_NAME in "${MODEL_NAMES[@]}"; do
-    ./eval_cl.sh $MODEL_NAME
-done
--- prepare_all_datasets_slim.sh
+++
@@ -1,5 +0,0 @@
-python scripts/datasets/prepare.py --dataset_family text-vqa-slim
-python scripts/datasets/prepare.py --dataset_family tally-qa-slim
-python scripts/datasets/prepare.py --dataset_family gqa-slim
-python scripts/datasets/prepare.py --dataset_family vqa-v2-slim
-python scripts/datasets/prepare.py --dataset_family refcoco-slim
